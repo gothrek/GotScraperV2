d3ce93e69b60a6caf3b6a2c0e11e919ac22bba54
Luca Pellegrino <RMPELLUC01@cef-farma.com>
2019-10-12 18:29:57 +0200

    Aggiungere i file di progetto.

---
+++ GotScraper/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(240, 123)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Test"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = Global.GotScraper.My.Resources.Resources.BarTop
+        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel1.Controls.Add(Me.RadioButton2)
+        Me.Panel1.Controls.Add(Me.RadioButton1)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(503, 426)
+        Me.Panel1.TabIndex = 1
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 241)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Directory"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 267)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(11, 293)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Scan"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 319)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Label2"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(360, 205)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(59, 17)
+        Me.RadioButton1.TabIndex = 5
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Italiano"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(360, 228)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(59, 17)
+        Me.RadioButton2.TabIndex = 6
+        Me.RadioButton2.Text = "Inglese"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(503, 426)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "GotScraper - Video Games Scraper x windows by Gothrek"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ GotScraper/Form1.vb
@@ -0,0 +1,196 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Xml
+
+Public Class Form1
+
+    Public Function GetCRC32(ByVal sFileName As String) As String
+        Try
+            Dim FS As FileStream = New FileStream(sFileName, FileMode.Open, FileAccess.Read, FileShare.Read, 8192)
+            Dim CRC32Result As Integer = &HFFFFFFFF
+            Dim Buffer(4096) As Byte
+            Dim ReadSize As Integer = 4096
+            Dim Count As Integer = FS.Read(Buffer, 0, ReadSize)
+            Dim CRC32Table(256) As Integer
+            Dim DWPolynomial As Integer = &HEDB88320
+            Dim DWCRC As Integer
+            Dim i As Integer, j As Integer, n As Integer
+
+            'Create CRC32 Table
+            For i = 0 To 255
+                DWCRC = i
+                For j = 8 To 1 Step -1
+                    If (DWCRC And 1) Then
+                        DWCRC = ((DWCRC And &HFFFFFFFE) \ 2&) And &H7FFFFFFF
+                        DWCRC = DWCRC Xor DWPolynomial
+                    Else
+                        DWCRC = ((DWCRC And &HFFFFFFFE) \ 2&) And &H7FFFFFFF
+                    End If
+                Next j
+                CRC32Table(i) = DWCRC
+            Next i
+
+            'Calcualting CRC32 Hash
+            Do While (Count > 0)
+                For i = 0 To Count - 1
+                    n = (CRC32Result And &HFF) Xor Buffer(i)
+                    CRC32Result = ((CRC32Result And &HFFFFFF00) \ &H100) And &HFFFFFF
+                    CRC32Result = CRC32Result Xor CRC32Table(n)
+                Next i
+                Count = FS.Read(Buffer, 0, ReadSize)
+            Loop
+            Return Hex(Not (CRC32Result))
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+    Public Function CercaArcadeDatabase(ByVal sFileName As String) As String
+        Dim strReq As String = "" 'Testo della richiesta/query
+        Dim strData As String = "" 'Testo recuperato dalla richiesta
+        Dim dataStream As Stream
+        Dim reader As StreamReader
+        Dim request As WebRequest
+        Dim response As WebResponse
+
+        Dim query As String = "query_mame&game_name="
+        Dim lingua As String = "it"
+
+        If RadioButton2.Checked Then
+            lingua = "en"
+        End If
+
+        strReq = "http://adb.arcadeitalia.net/service_scraper.php?ajax=" & query & sFileName & "&lang=" & lingua
+        'strReq = "http://www.progettoemma.net/index.php?gioco=4dwarrio"
+        request = WebRequest.Create(strReq)
+        'request.UseDefaultCredentials = True
+
+        Dim proxy As WebProxy = New WebProxy() '= request.Proxy
+        Dim proxyAddress As String = "proxy01.cef-farma.lan:3128"
+
+        Try
+            response = request.GetResponse()
+        Catch ex As Exception
+            'TODO gestire il proxy
+            'Dim newUri As Uri = New Uri(proxyAddress)
+
+            'proxy.Address = newUri
+            'proxy.Credentials = New NetworkCredential("RMPELLUC01", "N4tanN3ver", "Cef-farma.lan")
+            'proxy.UseDefaultCredentials = True
+
+            'request.Proxy = proxy
+            'MsgBox("La connessione richiede l'uso di un proxy!!")
+            'response = request.GetResponse()
+        End Try
+
+        dataStream = response.GetResponseStream()
+        reader = New StreamReader(dataStream)
+        strData = reader.ReadToEnd()
+        CercaArcadeDatabase = strData
+        reader.Close()
+        response.Close()
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim cartella As String = ""
+
+        FolderBrowserDialog1.ShowDialog()
+        cartella = FolderBrowserDialog1.SelectedPath
+        Label1.Text = cartella
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim FILE_NAME As String = "log.txt"
+        Dim sw As StreamWriter
+        Dim fs As FileStream = Nothing
+
+        Dim game As String = ""
+        Dim info As String = ""
+
+        Dim crc32 As String = ""
+        Dim contatore As Integer = 0
+        Dim contatoreScartati As Integer = 0
+
+        If File.Exists(FILE_NAME) = True Then 'se esite un file di log lo cancelliamo
+            File.Delete(FILE_NAME)
+        End If
+
+        fs = File.Create(FILE_NAME)
+        fs.Close()
+        sw = File.AppendText(FILE_NAME)
+
+        Dim inizio As DateTime = Now
+
+        For Each file As String In Directory.GetFiles(Label1.Text)
+
+            game = file.Substring(Label1.Text.Length + 1, file.Length - Label1.Text.Length - 5)
+            Label2.Text = game
+
+            info = CercaArcadeDatabase(game)
+            If info.Chars(11) <> "]" Then
+                crc32 = GetCRC32(file)
+
+                sw.WriteLine(game & " - " & crc32 & " - " & info)
+                contatore += 1
+            Else
+                game = file.Substring(Label1.Text.Length + 1, file.Length - Label1.Text.Length - 1)
+                sw.WriteLine(game & " - Fallito")
+                contatoreScartati += 1
+            End If
+        Next
+        Dim fine As DateTime = Now
+        sw.Close()
+
+        MsgBox("Scansione terminata! Elementi individuati:" & contatore & " in " & fine.Subtract(inizio).Seconds)
+
+    End Sub
+
+    Dim filePath As String = "Demo.xml"
+
+    Private Sub CreFileDemoXML(ByVal filePaths As String)
+        Dim Scrivi As New XmlTextWriter(filePaths, System.Text.Encoding.UTF8)
+        Scrivi.WriteStartDocument(True)
+        Scrivi.Formatting = Formatting.Indented
+        Scrivi.Indentation = 2
+        Scrivi.WriteStartElement("Articoli_Table")
+        createNodo(1, "172-32-1176", "TOSHIBA", "e-STUDIO456SE", "Multifunzione TOSHIBA a e-STUDIO456SE", True, Scrivi)
+        createNodo(2, "172-32-1174", "TOSHIBA", "e-STUDIO356SE", "Multifunzione TOSHIBA a e-STUDIO356SE", False, Scrivi)
+        Scrivi.WriteEndElement()
+        Scrivi.WriteEndDocument()
+        Scrivi.Close()
+    End Sub
+
+    Private Sub createNodo(ByVal pId As String, ByVal pCode As String, ByVal pMarca As String,
+                              ByVal pModello As String, ByVal pDescrizione As String,
+                              ByVal pOfferta As Boolean, ByVal scrivi As XmlTextWriter)
+        scrivi.WriteStartElement("articolo")
+        scrivi.WriteStartElement("Id")
+        scrivi.WriteString(pId)
+        scrivi.WriteEndElement()
+        scrivi.WriteStartElement("Codice")
+        scrivi.WriteString(pCode)
+        scrivi.WriteEndElement()
+        scrivi.WriteStartElement("Marca")
+        scrivi.WriteString(pMarca)
+        scrivi.WriteEndElement()
+        scrivi.WriteStartElement("Modello")
+        scrivi.WriteString(pModello)
+        scrivi.WriteEndElement()
+        scrivi.WriteStartElement("Descrizione")
+        scrivi.WriteString(pDescrizione)
+        scrivi.WriteEndElement()
+        scrivi.WriteStartElement("Offerta")
+        scrivi.WriteString(pOfferta)
+        scrivi.WriteEndElement()
+        scrivi.WriteEndElement()
+    End Sub
+
+    Private Sub ReadFileXML_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'Creo il file XML Demo.xml che contiene i valori
+        CreFileDemoXML(filePath)
+    End Sub
+End Class
